7d8009390953e8a7e0325ac7fb7ed6b67c959eb6
Aashray <aashray446@gmail.com>
2022-05-05 21:14:41 +0530

    <DAY5 : [Solved pattern based recursion question and bubble short in recursion]>

---
+++ bubble_short.java
@@ -0,0 +1,60 @@
+public class bubble_short {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,6,5,0,3,4,6}; 
+        bubble_short_recur(arr, true);
+        for (int i : arr) {
+            System.out.print(i + ", ");
+        }
+    }
+    
+    static void regular_bubble_short(int[] arr){
+        int tmp = 0;
+        boolean is_swapped = true;
+        int position = 0;
+        while(is_swapped){
+            is_swapped = false;
+            if(position+1 == arr.length-1){
+                position = 0;
+            }
+           while(position != arr.length-2){
+            if( arr[position] > arr[position+1] ) {
+                tmp = arr[position];
+                arr[position] = arr[position+1];
+                arr[position+1] = tmp;
+                is_swapped = true;
+            }
+            position++;
+           }
+           position = 0;
+        }
+    }
+
+    static int current_pos = 0;
+    static void bubble_short_recur(int[] arr, boolean is_changed) {
+      
+
+        if(current_pos == arr.length - 2) {
+            if(is_changed == false) {
+                return;
+            }
+            is_changed = false;
+            current_pos = 0;
+        }
+
+        if(arr[current_pos] > arr[current_pos + 1]){
+            swap(arr, current_pos);
+            is_changed = true;
+        }   
+        current_pos = current_pos +1;
+        bubble_short_recur(arr, is_changed);
+       
+
+    }
+
+    static void swap(int[] arr, int pos){
+        int tmp = arr[pos];
+        arr[pos] = arr[pos+1];
+        arr[pos+1] = tmp; 
+    }
+
+}
--- pattern_with_recursion.java
+++ pattern_with_recursion.java
@@ -1,7 +1,7 @@
 public class pattern_with_recursion {
     
     public static void main(String[] args) {
-        print_recur(5, 5);
+        print_recur(5,5);
     }
 
     public static void print_pattern(int i, int j) {
@@ -16,18 +16,39 @@ public class pattern_with_recursion {
     }
     public static void print_recur(int i , int j) {
 
-        if(i == 0 ) {
-            return;
-        }
+       
         if(j != 0) {
             System.out.print("*");
             print_recur(i, j-1);
         }else {
+            if(i == 0 ) {
+                return;
+            }
             System.out.println();
-            print_pattern(i-1, j=i-1);
+            print_recur(i-1, i-1);
         }
      
 
+    }
+
+    static void print_rec_2(int i, int j) {
+
+        if(i == 6) {
+            return;
+        }
+
+        if( j != 0) {
+            System.out.print("*");
+            print_rec_2(i, j-1);
+        }
+        else {
+            System.out.println();
+            i = i + 1;
+            j = i;
+            print_rec_2(i, j);
+        }
+  
+        
     }
 
 }
